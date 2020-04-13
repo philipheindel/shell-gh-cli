d3e8aacce0ecd2a5a57bb587452d0203afc6a2e4
Philip Heindel <p.e.heindel@gmail.com>
2020-04-13 13:39:02 -0400

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo gh cli
+function gh_cli() {
+  if [[ $1 == "issue" ]]
+  then
+  
+  elif [[ $1 == "pr" ]]
+  then
+  
+  elif [[ $1 == "repo" ]]
+  then
+  
+  elif [[ $1 == "help" ]]
+  then
+   
+  else
+    echo "$1" is an invalid argument
+  fi
+}
+
+alias gh="gh_cli()"
+
