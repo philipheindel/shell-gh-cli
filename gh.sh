a6007f8140cf46c9b02a8883c18a9ac335b218df
Philip Heindel <p.e.heindel@gmail.com>
2020-04-13 14:38:52 -0400

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config --global user.username;
+git config --global user.username
 
 gh_issue() {
 
@@ -21,20 +21,20 @@ gh_help() {
 gh_cli() {
   if [ "$1" = "issue" ] ;
   then
-    gh_issue() $2
+    gh_issue $2
   elif [ "$1" = "pr" ] ;
   then
-    gh_pr() $2
+    gh_pr $2
   elif [ "$1" = "repo" ] ;
   then
-    gh_repo() $2
+    gh_repo $2
   elif [ "$1" = "help" ] ;
   then
-    gh_help() $2
+    gh_help $2
   else
     echo "$1" is an invalid argument
   fi
 }
 
-alias gh="gh_cli()"
+alias gh="gh_cli"
 
