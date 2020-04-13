da95c8c062eb7cd11931999711358b2288fe1ecf
Philip Heindel <p.e.heindel@gmail.com>
2020-04-13 14:03:19 -0400

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+git config --global user.username
+
+function gh_issue() {
+
+}
+
+function gh_pr() {
+
+}
+
+function gh_repo() {
+
+}
+
+function gh_help() {
+
+}
+
 function gh_cli() {
-  if [[ $1 == "issue" ]]
+  if [ $1 = "issue" ];
   then
-  
-  elif [[ $1 == "pr" ]]
+    gh_issue() $2
+  elif [ $1 = "pr" ];
   then
-  
-  elif [[ $1 == "repo" ]]
+    gh_pr() $2
+  elif [ $1 = "repo" ];
   then
-  
-  elif [[ $1 == "help" ]]
+    gh_repo() $2
+  elif [ $1 = "help" ];
   then
-   
+    gh_help() $2
   else
     echo "$1" is an invalid argument
   fi
