10eb223eb02c98783e368a1e99e03ef0d7be600f
Philip Heindel <p.e.heindel@gmail.com>
2020-04-13 15:18:08 -0400

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -21,20 +21,21 @@ gh_help() {
 gh_cli() {
   if [ "$1" = "issue" ]
   then
-    gh_issue $2
+    gh_issue ${@:2}
   elif [ "$1" = "pr" ]
   then
-    gh_pr $2
+    gh_pr ${@:2}
   elif [ "$1" = "repo" ]
   then
-    gh_repo $2
+    gh_repo ${@:2}
   elif [ "$1" = "help" ]
   then
-    gh_help $2
+    gh_help ${@:2}
   else
     echo "$1" is an invalid argument
   fi
 }
 
-alias gh="gh_cli"
+# alias gh="gh_cli"
 
+gh_cli $1
